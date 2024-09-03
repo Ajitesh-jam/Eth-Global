f4537786e1b90f832fb745770455a970afecf78b
Ajitesh-jam <ajitesh.jam@kgpian.iitkgp.ac.in>
2024-09-03 12:55:02 +0530

    Stackr Labs

---
+++ .history/Stackr Labs/Eth_Global/src/stackr/mru_20240903001544.ts
@@ -0,0 +1,31 @@
+import { MicroRollup } from "@stackr/sdk";
+import { stackrConfig } from "../../stackr.config";
+import { machine } from "./machine";
+import { UpdateCounterSchema } from "./schemas";
+
+const mru = await MicroRollup({
+  config: stackrConfig,
+  actionSchemas: [UpdateCounterSchema],
+  stateMachines: [machine],
+});
+
+await mru.init();
+import { Playground } from "@stackr/sdk/plugins";
+ 
+const rollup = await MicroRollup({
+    config: stackrConfig,
+    actionSchemas: [], // Action Schemas of the rollup.
+    stateMachines: [machine],
+});
+ 
+await rollup.init();
+
+
+ 
+Playground.init(rollup); 
+
+
+
+export { mru };
+
+
---
+++ .history/Stackr Labs/Eth_Global/stackr.config_20240903001431.ts
@@ -0,0 +1,38 @@
+import { DA, KeyPurpose, SignatureScheme, StackrConfig } from "@stackr/sdk";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const stackrConfig: StackrConfig = {
+  sequencer: {
+    blockSize: 16,
+    blockTime: 10,
+    allowEmptyBlocks: true,
+  },
+  syncer: {
+    slotTime: 1000,
+    vulcanRPC: process.env.VULCAN_RPC as string,
+    L1RPC: process.env.L1_RPC as string,
+  },
+  operator: {
+    accounts: [
+      {
+        privateKey: process.env.PRIVATE_KEY as string,
+        purpose: KeyPurpose.BATCH,
+        scheme: SignatureScheme.ECDSA,
+      },
+    ],
+  },
+  domain: {
+    name: "Stackr MVP v0",
+    version: "1",
+    salt: "0x0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef",
+  },
+  datastore: {
+    type: "sqlite",
+    uri: process.env.DATABASE_URI,
+  },
+  registryContract: REGISTRY_CONTRACT,
+  preferredDA: DA.AVAIL,
+  logLevel: "log",
+};
