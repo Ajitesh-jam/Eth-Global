c4782863464e8601e6c68a2d920a2d90c3b489f3
Ajitesh-jam <ajitesh.jam@kgpian.iitkgp.ac.in>
2024-09-06 19:57:47 +0530

    Update State machine Stackr Labs

---
+++ .history/Stackr Labs/Eth_Global/src/stackr/machine_20240906192007.ts
@@ -0,0 +1,17 @@
+import { StateMachine } from "@stackr/sdk/machine";
+
+import * as genesisState from "../../genesis-state.json";
+import { skinOwners} from "./state";
+import { transitions } from "./transitions";
+
+
+const machine = new StateMachine({
+  id: "SkinOwners",
+  stateClass:skinOwners,
+  initialState: genesisState.array,
+  on: transitions,
+});
+
+export { machine };
+
+
